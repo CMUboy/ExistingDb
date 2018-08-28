4d6cf9c977e94787038a4fd040c11641735df99a
Joseph Ku <joseph.ku@octanner.com>
2018-08-28 10:53:21 -0600

    Chapter 18: Manually Modeling a Database

---
+++ Models/Manual/ShoeWidth.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace ExistingDb.Models.Manual {
+
+    public class ShoeWidth {
+
+        public long UniqueIdent { get; set; }
+
+        public string WidthName { get; set; }
+        
+        public IEnumerable<Shoe> Products { get; set; }
+    }
+}
